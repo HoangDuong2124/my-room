e19a97a2b6139f484c7a62474e11cb4ca7ffdb9d
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2024-01-23 09:23:52 +0700

    fix: room item

--- app/room/RoomItem.tsx
+++ app/room/RoomItem.tsx
@@ -3,20 +3,22 @@ import React from 'react'
 
 const RoomItem = ({name}:{name:string}) => {
     return (
-        <div className='p-2 hover:bg-[Gainsboro]'>
-            <button className='flex items-center w-full '>
-                    <Image
-                        className=' rounded-full mr-1'
-                        src="/img/avatar.jpg"
-                        width={40}
-                        height={50}
-                        alt="Avartar default"
-                    />
+        // <div className='p-2 hover:bg-[Gainsboro]'>
+        //     <button className='flex items-center w-full '>
+        //             <Image
+        //                 className=' rounded-full mr-1'
+        //                 src="/img/avatar.jpg"
+        //                 width={40}
+        //                 height={50}
+        //                 alt="Avartar default"
+        //             />
 
-                <p className='truncate'>{name}</p>
-            </button>
+        //         <p className='truncate'>{name}</p>
+        //     </button>
 
-        </div>
+        // </div>
+
+        <></>
     )
 }
 
