142e8425cec38029d99b7ddc7a2e31bf0672f366
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2024-02-28 10:07:21 +0700

    update css

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,6 @@
 export const LoadingMess = () => {
   return (
-    <div className=" w-screen h-screen bg-slate-100 flex items-center justify-center">
+    <div className=" w-[75%] h-screen bg-slate-100 flex items-center justify-center">
       <div className="sharingan">
         <div className="inner-ring">
           <div className="tomoe"></div>
@@ -13,3 +13,18 @@ export const LoadingMess = () => {
   );
 };
 
+export const LoadingRoom = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="wrapper">
+      <div className="circle2"></div>
+      <div className="circle2"></div>
+      <div className="circle2"></div>
+      <div className="shadow2"></div>
+      <div className="shadow2"></div>
+      <div className="shadow2"></div>
+    </div>
+    </div>
+    
+  );
+};
